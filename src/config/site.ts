84898d19d810f2b94dd5c59b790a50401a290f4d
John Mark Marquez <jhnmrkmrqz@Johns-MacBook-Air.local>
2026-07-07 14:22:32 +0800

    chore: initial commit and setup

---
+++ src/config/site.ts
@@ -0,0 +1,22 @@
+export const siteConfig = {
+    title: "John Mark & Sandra",
+  
+    projectName: "Project Reverie",
+  
+    wedding: {
+      date: new Date("2028-02-10T14:00:00+08:00"),
+      ceremony: {
+        venue: "Don Bosco Chapel on the Hill",
+        time: "2:00 PM",
+      },
+      reception: {
+        venue: "Narra Hill",
+        time: "5:00 PM",
+      },
+    },
+  
+    theme: {
+      watercolor: true,
+      paperTexture: true,
+    },
+  } as const;
